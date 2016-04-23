5b56af27e5c48ef4535c559b1e217c67dafc9f36
Daniel <daniel.maansson@gmail.com>
2016-04-23 18:00:00 +0200

    Jumpy with keyboard

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -15,6 +15,8 @@ public class Player : MonoBehaviour
 	[SerializeField]
 	public float m_pushPower = 5f;
 
+	public bool m_useKeyboard = false;
+
 	Controller m_controller;
 	Rigidbody2D m_body;
 	Vector2 m_forceVector = Vector2.zero;
@@ -34,17 +36,50 @@ public class Player : MonoBehaviour
 	
 	void Update ()
 	{
-		if (m_controller.GetButtonDown(Xbox360ControllerButtonId.A))
+		var left = Vector2.zero;
+		var right = Vector2.zero;
+		bool jumpButton = false;
+
+		if (!m_useKeyboard)
 		{
+			left = m_controller.GetJoystick(Xbox360ControllerJoystickId.Left);
+			right = m_controller.GetJoystick(Xbox360ControllerJoystickId.Right);
+			jumpButton = m_controller.GetButtonDown(Xbox360ControllerButtonId.RB) && m_cooldown < 0f;
+		}
+		else
+		{
+			if (Input.GetKey(KeyCode.W))
+				left += Vector2.up;
+			if (Input.GetKey(KeyCode.S))
+				left += Vector2.down;
+			if (Input.GetKey(KeyCode.A))
+				left += Vector2.left;
+			if (Input.GetKey(KeyCode.D))
+				left += Vector2.right;
+
+			if (left.magnitude > 0.1f)
+				left.Normalize();
+
+			if (Input.GetKey(KeyCode.UpArrow))
+				right += Vector2.up;
+			if (Input.GetKey(KeyCode.DownArrow))
+				right += Vector2.down;
+			if (Input.GetKey(KeyCode.LeftArrow))
+				right += Vector2.left;
+			if (Input.GetKey(KeyCode.RightArrow))
+				right += Vector2.right;
+
+			if (right.magnitude > 0.1f)
+				right.Normalize();
+
+			jumpButton = Input.GetKeyDown(KeyCode.Space) && m_cooldown < 0f;
 		}
 
-		var left = m_controller.GetJoystick(Xbox360ControllerJoystickId.Left);
 		m_forcePointIndicator.transform.position = transform.position + (Vector3)left * transform.localScale.x * 0.3f + Vector3.back;
 
-		var right = m_controller.GetJoystick(Xbox360ControllerJoystickId.Right);
 		m_forceVector = right;
 
-		if (m_controller.GetButtonDown(Xbox360ControllerButtonId.RB) && m_cooldown < 0f)
+		if (jumpButton)
 		{
 			m_body.AddForceAtPosition(m_forceVector * m_maxJumpPower, (Vector2)m_forcePointIndicator.transform.position, ForceMode2D.Impulse);
 			m_cooldown = 0.7f;
